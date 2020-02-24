a291d08cd026af8c3804cc661c882b74e9c176ed
Brandon Campbell-Kearns <bk@brandonkearns.com>
2020-02-24 10:40:40 -1000

    Adds classroom object for cohort 001

--- function-practice.js
+++ function-practice.js
@@ -1,3 +1,17 @@
 // today we will continue practicing functions based on feedback
 // from friday's retro #agile
 
+var classroom = {
+  name: "Aloha Spark Lab",
+  location: "Pearl Harbor-Hickam AF Base",
+  maxNumberOfStudents: 6,
+  numberOfInstructors: 2,
+  students: [
+    {firstName: "Tom", lastName: "Schaum"},
+    {firstName: "Carol", lastName: "Grande"},
+    {firstName: "David", lastName: "Allshouse"},
+    {firstName: "Micah", lastName: "Johns"},
+    {firstName: "Mitchell", lastName: "Elias"},
+    {firstName: "Marissa", lastName: "Sulon"},
+  ]
+}
